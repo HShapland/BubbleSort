696a204b858edd2203fa7397478335e3f0efc3f3
Mohammed Ali Butt <abutt2001@hotmail.com>
2023-03-10 15:31:55 +0000

    bubble sort skeleton

---
+++ BubbleSortLab/BubbleSort.Test/BubbleSortTests.cs
@@ -0,0 +1,16 @@
+namespace BubbleSort.Test
+{
+    public class BubbleSortTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
