6d44f0ad2f1e6f26c4db0a920b650fbcda8980ac
Mohammed Ali Butt <abutt2001@hotmail.com>
2023-03-10 15:47:41 +0000

    pushing skeleton

--- BubbleSortLab/BubbleSort.App/BubbleSort.cs
+++ BubbleSortLab/BubbleSort.App/BubbleSort.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BubbleSort.App;
 
-namespace BubbleSort.App
+public class BubbleSort
 {
-    internal class BubbleSort
+    public static int[] SortArray(int[] array)
     {
+        return array;
     }
 }
--- BubbleSortLab/BubbleSort.Test/BubbleSortTests.cs
+++ BubbleSortLab/BubbleSort.Test/BubbleSortTests.cs
@@ -1,16 +1,24 @@
-namespace BubbleSort.Test
-{
-    public class BubbleSortTests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+using BubbleSort.App;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+namespace BubbleSort.Test;
+
+public class BubbleSortTests
+{
+    [TestCaseSource(nameof(array))]
+    public void SortArray_WhenGivenAnArrayOfInts_ReturnSortedArray(int[] array)
+    {
+        // Arrange
+        int[] expected = expectedResult;
+
+        // Act
+        var result = BubbleSort.SortArray(array);
+
+        // Assert
+        Assert.Pass();
     }
-}
+
+    public static int[] array = { 5, 3, 2, 4, 1 };
+    public static int[] expectedResult = { 1, 2, 3, 4, 5 };
+
+
+    }
